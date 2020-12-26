9ff999aa465eda592ad1a895348d655d8f7b021c
Michael <michael@qbitone.de>
2020-12-26 13:42:13 +0100

    dev env

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const postCSSPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        path: path.resolve(__dirname, 'app'),
+        filename: 'bundled.js'
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: [
+                'style-loader',
+                'css-loader?url=false',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: postCSSPlugins
+                        }
+                    }
+                }
+            ]
+        }]
+    }
+};
